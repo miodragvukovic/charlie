bce144fb7ce97eedbb4c17b5d4102f3bb1ef3a28
miodrag.vukovic <vukovic.development@gmail.com>
2019-01-09 12:35:32 +0100

    going online

--- script.js
+++ script.js
@@ -1,25 +1,126 @@
-var introStructure = new TimelineLite()
-	introStructure
-	.staggerTo('.topline', 0.2, {width: "100%"}, .15, "stagger")
-	.to('.right-upper', .1, {height: "100%"})
-	.to('.right-lower', .1, {height: "100%"})
-	.to('.bottomline-2', .2, {width: "100%"})
-	.to('.bottomline-1', .2, {width: "100%"})
-	.to('.left', .2, {height: "100%"})
-	introStructure.delay(0.3)
-var introStructureText = new TimelineLite()
-	introStructureText
-	.to('.link-1', 0.5, {opacity: 1}, 0.5)
-	.to('.link-2', 0.5, {opacity: 1}, 0.6)
-	.to('.link-wrap', 0.5, {opacity: 1}, 0.9)
-	.to('#title', 0.3, {top: 0}, 1.1)
-var introStructureButtont = new TimelineLite()
-	introStructureButtont
-	.staggerTo('.menu span', 0.3, {width: "100%"}, 0.1, "stagger")
-	introStructureButtont.delay(0.5)
-var introStructureLinks = new TimelineLite()
-	introStructureLinks
-	.to('.nav-3', 0.2, {opacity: 1})
-	.to('.nav-2', 0.2, {opacity: 1})
-	.to('.nav-1', 0.2, {opacity: 1})
-	introStructureLinks.delay(1.6)
+// GLOBALNE VARIJABLE
+var navbar = document.getElementById('navbar')
+var site = document.querySelector('main')
+var siteWidth
+var siteHeight
+var difference
+
+
+// SWIPER
+var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    autoplay: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+})
+
+// ONLOAD FUNKCIJE
+window.onload = function () {
+	window.scrollTo(0,0)
+}
+setTimeout(function(){
+	siteWidth = window.innerWidth
+	siteHeight = document.body.offsetHeight - window.innerHeight
+	difference = siteWidth / siteHeight
+	document.getElementById('overlay').style.opacity = "0"
+}, 1000)
+setTimeout(function(){
+	document.querySelector('.h1').classList.add('active')
+	document.querySelector('.h3').classList.add('active')
+	navbar.classList.remove('inactive')
+	document.getElementById('overlay').style.display = "none"
+
+}, 1500)
+
+
+// AKORIDIONI SEKCIJE PITANJA/ODGOVORI
+for (var q = 0; q < document.getElementsByClassName('question').length; q++) {
+	document.getElementsByClassName('question')[q].addEventListener('click', function(){
+		var question = document.getElementsByClassName('question')[q]
+		for ( var z = 0; z < document.getElementsByClassName('answer').length; z++ ) {
+			document.getElementsByClassName('answer')[z].classList.remove('active')
+		}
+		for ( var f = 0; f < document.getElementsByClassName('question').length; f++ ) {
+			document.getElementsByClassName('question')[f].classList.remove('active')
+		}
+		this.classList.add('active')
+		this.nextElementSibling.classList.add('active')
+	})
+}
+
+
+// KONSTRUISANJE GALERIJE
+var string = ""
+document.querySelector('.gallery-trigger').addEventListener('click', function(){
+	var imageList = document.getElementById('image-list')
+	string = "<a data-fancybox='gallery' href='images/img1.jpg'></a><a data-fancybox='gallery' href='images/img2.jpg'></a><a data-fancybox='gallery' href='images/img3.jpg'></a>"
+	imageList.innerHTML = string
+	imageList.children[0].click()
+})
+
+
+// SVE U VEZI MODALA/OVERLAY
+document.onkeydown = function(e) {
+    if (e.keyCode == 27) {
+        for (var h = 0; h < document.getElementsByClassName('overlay').length; h++) {
+        	document.getElementsByClassName('overlay')[h].classList.remove('active')
+        }
+    }
+}
+for (var h = 0; h < document.getElementsByClassName('overlay').length; h++) {
+	document.getElementsByClassName('overlay')[h].addEventListener('click', function(e){
+		if ( e.target != this.children[1] || e.target == this.children[0] ) {
+			this.classList.remove('active')
+		}
+	})
+}
+for (var r = 0; r < document.getElementsByClassName('map-trigger').length; r++) {
+	document.getElementsByClassName('map-trigger')[r].addEventListener('click', function(){
+		mapVal = this.getAttribute('data-value')
+		document.querySelector("."+ mapVal +"").classList.add('active')
+	})
+}
+
+
+// SVI ON SCROLL HANDLERI
+window.addEventListener('scroll', function(){
+	var siteTop = window.pageYOffset
+	if ( siteTop > 30 ) {
+		site.classList.add('active')
+		navbar.classList.add('scrolled')
+	} else {
+		navbar.classList.remove('scrolled')
+		site.classList.remove('active')
+	}
+	for ( var i = 0; i < document.getElementsByClassName('section').length; i ++ ) {
+		var sec = document.getElementsByClassName('section')[i]
+		var pageBottom = window.innerHeight + window.pageYOffset
+		var elemHeight = sec.clientHeight / 1.3
+		if ( window.innerWidth < 1366 ) {
+			elemHeight = sec.clientHeight / 2
+		}
+		if ( pageBottom >= sec.offsetTop + elemHeight ) {
+			sec.classList.add('active')
+			var secVal = sec.children[0].getAttribute('data-value')
+			
+		} else {
+			sec.classList.remove('active')
+		}
+		if ( document.querySelector('.gallery').classList.contains('active') ) {
+			document.querySelector('.gym').classList.add('font-color')
+		} else {
+			document.querySelector('.gym').classList.remove('font-color')
+		}
+	}
+	document.getElementById('progress').style.width = ""+difference * siteTop+"px"
+	for ( var v = 0; v < document.getElementsByClassName('link').length; v++ ) {
+		document.getElementsByClassName('link')[v].classList.remove('active')
+		if ( secVal == undefined ) {
+			document.querySelector('.home-logo').classList.add('active')
+		} else {
+			document.querySelector('.home-logo').classList.remove('active')
+			document.querySelector("."+secVal+"").classList.add('active')
+		}
+	}
+})
